9a4685653731f32b676f422db0fe774acb626532
Bhardwajnishtha <nishthab309@gmail.com>
2026-06-09 19:46:16 +0530

    Update code

--- Question35.java
+++ Question35.java
@@ -1,13 +1,1 @@
-public class Question35 {
-    public static void main(String[] args) {
-        char i, j, n;
-        n = 'E';
-        for(i = 'A'; i <= 'E'; i++) {
-            for(j = 'A'; j <= i; j++) {
-                System.out.print(i);
-            }
-            System.out.println();
-        }
-    }
-    
-}
+c
