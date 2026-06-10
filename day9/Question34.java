697ca167bab448463e53e92866ce98cd5df306e5
Bhardwajnishtha <nishthab309@gmail.com>
2026-06-10 10:13:15 +0530

    Updated code

---
+++ day9/Question34.java
@@ -0,0 +1,17 @@
+package day9;
+
+public class Question34 {
+    public static void main(String[] args) {
+        int i, j, n;
+        n = 5;
+        for(i=5;i>=1;i--){
+            for(j=1;j<=i;j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
